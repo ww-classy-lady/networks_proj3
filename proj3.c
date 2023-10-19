ae824f38df47fda424585287067c6752d0261972
Wendy Wu <96844488+ww-classy-lady@users.noreply.github.com>
2023-10-18 20:35:24 -0400

    nothing changed, just wanna make sure it is the latest

--- proj3.c
+++ proj3.c
@@ -249,130 +249,97 @@ int sockets(){
     if (listen (sd, QLEN) < 0)
         errexit ("cannot listen on port %s\n", port);
 /* accept, parse, write, and close(sd2) can be repeated to accept more connections from the listening socket.*/
+    addrlen = sizeof (addr);
     /* accept a connection */
     while(true)
     {
-    addrlen = sizeof (addr);
-    sd2 = accept (sd,&addr,&addrlen);
-    if (sd2 < 0)
-        errexit ("error accepting connection", NULL);
-    /* Parse the http request to see what we do with it*/
-    /* parse the http request using read */
-    int totalSize = 0; //temporarily set the totalSize of the http Request to BUFLEN
-    httpRequest = (char *)malloc(BUFLEN); //allocate size BUFLEN for http Request
-    httpRequest[0] = '\0'; //null terminate http request
-    int infoRead = 0;
-    char store[BUFLEN]; //temporary storage
-    /* Get the header by getting piece by piece and temporarily store it in store, then transfer to httpRequest*/
-    while((infoRead = read(sd2, store, sizeof(store))) > 0)  //as long as sp has the request, store request in httpRequest
-    {
-        /* dynamically allocate more space to httpRequest if needed */
-        if(totalSize + infoRead >= totalSize - 1) //if the existing request plus the new string exceed allocated space, allocate more
+        sd2 = accept (sd,&addr,&addrlen);
+        if (sd2 < 0)
+            errexit ("error accepting connection", NULL);
+        /* Parse the http request to see what we do with it*/
+        /* parse the http request using read */
+        int totalSize = 0; //temporarily set the totalSize of the http Request to BUFLEN
+        httpRequest = (char *)malloc(BUFLEN); //allocate size BUFLEN for http Request
+        httpRequest[0] = '\0'; //null terminate http request
+        int infoRead = 0;
+        char store[BUFLEN]; //temporary storage
+        /* Get the header by getting piece by piece and temporarily store it in store, then transfer to httpRequest*/
+        while((infoRead = read(sd2, store, sizeof(store))) > 0)  //as long as sp has the request, store request in httpRequest
         {
-            totalSize = totalSize + infoRead; //allocate another BUFLEN size of space to store string
-            httpRequest = (char *)realloc(httpRequest, totalSize); //realloc more space
-        } 
-        strncat(httpRequest, store, infoRead); //transfer the string from store to httpRequest by infoRead number of bytes
-        if(strstr(httpRequest, rn)!=NULL) //check if we reached the end of the http request since it means terminated by \r\n
-        //and if we have \r\n at the end we can end reading
-        //from professor's simplication we can always assume it ends with \r\n 
-        { 
-            break;
+            /* dynamically allocate more space to httpRequest if needed */
+            if(totalSize + infoRead >= totalSize - 1) //if the existing request plus the new string exceed allocated space, allocate more
+            {
+                totalSize = totalSize + infoRead; //allocate another BUFLEN size of space to store string
+                httpRequest = (char *)realloc(httpRequest, totalSize); //realloc more space
+            } 
+            strncat(httpRequest, store, infoRead); //transfer the string from store to httpRequest by infoRead number of bytes
+            if(strstr(httpRequest, rn)!=NULL) //check if we reached the end of the http request since it means terminated by \r\n
+            //and if we have \r\n at the end we can end reading
+            //from professor's simplication we can always assume it ends with \r\n 
+            { 
+                break;
+            }
         }
-    }
-    
-    //printf("%s", httpRequest); //at this point the httpRequest will be complete
-    res = NULL; //initialize message to null for now;
-    //prinf("does httprequest have '\r\n\r\n'?%s", strstr(httpRequest, "\r\n"));
-    /* list of priority for HTTP response only return 1*/
-    /*
-    1. 400 Malformed Request
-    2. 501 Protocol Not Implemented
-    3. 405 Unsupported Method
-    4. SHUTDOWN requests:
-        (a) 200 Server Shutting Down
-        (b) 403 Operation Forbidden
-    5. GET requests:
-        (a) 406 Invalid Filename
-        (b) 200 OK
-        (c) 404 File Not Found
-    */
+        res = NULL; //initialize message to null for now;
    //while true was here
-    if(check400(httpRequest))
+        if(check400(httpRequest))
         { /* 400 malformed request check */ //need more testing but pass the test case on webpage
             res = "HTTP/1.1 400 Malformed Request\r\n\r\n";
-             /* write message to the connection */
+            /* write message to the connection */
             if(write (sd2, res,strlen (res)) < 0)
                 errexit ("error writing message: %s", res);
             break; //go straight to write message and end this current connection 
         }
-    else{
-        parseHTTP(); //parse method argument, httpversion
-        if(!isHTTP(httpVersion))
-        {
-            res = "HTTP/1.1 501 Protocol Not Implemented\r\n\r\n";
-            /* write message to the connection */
-            if(write (sd2, res,strlen (res)) < 0)
-                errexit ("error writing message: %s", res);
-            break;
-        }
-        else
-        {
-            if(!getOrShutdown(method))
+        else{
+            parseHTTP(); //parse method argument, httpversion
+            if(!isHTTP(httpVersion))
             {
-                //if not get or shutdown in the method
-                res = "HTTP/1.1 405 Unsupported Method\r\n\r\n";
+                res = "HTTP/1.1 501 Protocol Not Implemented\r\n\r\n";
                 /* write message to the connection */
                 if(write (sd2, res,strlen (res)) < 0)
                     errexit ("error writing message: %s", res);
                 break;
-            }  
-            else if(strcmp(method, "SHUTDOWN") == 0)
-            {
-                //close (sd);
-                //exit (0);
-                if(strcmp(argument, auth_token) == 0)
-                {
-                    res = "HTTP/1.1 200 Server Shutting Down\r\n\r\n";
-                    if(write (sd2, res,strlen (res)) < 0)
-                        errexit ("error writing message: %s", res);
-                    close(sd2);
-                    close(sd);
-                    exit(0);
-                    break; //not needed perhaps
-                }
-                else{
-                    res = "HTTP/1.1 403 Operation Forbidden\r\n\r\n";
-                    if(write (sd2, res,strlen (res)) < 0)
-                        errexit ("error writing message: %s", res);
-                    break;
-                }
             }
-            else if(strcmp(method, "GET") == 0)
+            else
             {
-                //step 1:
-                //1. When the requested file does not begin with a “/”, a minimal HTTP response of “HTTP/1.1 406
-                //Invalid Filename\r\n\r\n” must be returned.
-                if(!doesSlashBegin(argument))
+                if(!getOrShutdown(method))
                 {
-                    res = "HTTP/1.1 406 Invalid Filename\r\n\r\n";
+                    //if not get or shutdown in the method
+                    res = "HTTP/1.1 405 Unsupported Method\r\n\r\n";
                     /* write message to the connection */
                     if(write (sd2, res,strlen (res)) < 0)
                         errexit ("error writing message: %s", res);
                     break;
-                }
-                else
+                }  
+                else if(strcmp(method, "SHUTDOWN") == 0)
                 {
-                    printf("%s\n", document_directory);
-                    if(strcmp(argument, "/") == 0)
+                    //close (sd);
+                    //exit (0);
+                    if(strcmp(argument, auth_token) == 0)
                     {
-                        loc = fopen(strcat(document_directory, def), "r"); //check the default homepage.html
+                        res = "HTTP/1.1 200 Server Shutting Down\r\n\r\n";
+                        if(write (sd2, res,strlen (res)) < 0)
+                            errexit ("error writing message: %s", res);
+                        close(sd2);
+                        close(sd);
+                        exit(0);
+                        break; //not needed perhaps
                     }
                     else{
-                        loc = fopen(strcat(document_directory, argument), "r");
-                    }   
-                    if(loc == NULL) {   
-                        res = "HTTP/1.1 404 File Not Found\r\n\r\n";
+                        res = "HTTP/1.1 403 Operation Forbidden\r\n\r\n";
+                        if(write (sd2, res,strlen (res)) < 0)
+                            errexit ("error writing message: %s", res);
+                        break;
+                    }
+                }
+                else if(strcmp(method, "GET") == 0)
+                {
+                    //step 1:
+                    //1. When the requested file does not begin with a “/”, a minimal HTTP response of “HTTP/1.1 406
+                    //Invalid Filename\r\n\r\n” must be returned.
+                    if(!doesSlashBegin(argument))
+                    {
+                        res = "HTTP/1.1 406 Invalid Filename\r\n\r\n";
                         /* write message to the connection */
                         if(write (sd2, res,strlen (res)) < 0)
                             errexit ("error writing message: %s", res);
@@ -380,37 +347,54 @@ int sockets(){
                     }
                     else
                     {
-                        //200 ok land
-                        res = "HTTP/1.1 200 OK\r\n\r\n";
-                        if(write (sd2, res,strlen (res)) < 0)
-                            errexit ("error writing message: %s", res);
-                        char buffer[BUFLEN]; //temporary buffer to store binary data pieces
-                        size_t bufferSize = sizeof(buffer);
-                        size_t bytesRead = 0; //keep track of the bytesRead
-                        while(!feof(loc)) //as long as we are not at the end of the file for socket descriptor, binary data need to be continuous read and written
+                        printf("%s\n", document_directory);
+                        if(strcmp(argument, "/") == 0)
                         {
-                            bytesRead = fread(buffer, 1, bufferSize, loc); //read one byte of data from socket descriptor file
-                            if(bytesRead > 0) //got info to write because bytesRead is greater than 0
-                            {
-                                send(sd2, buffer, bytesRead, 0); //write 1 byte of data to file
-                            }   
+                            loc = fopen(strcat(document_directory, def), "r"); //check the default homepage.html
                         }
-                        fclose(loc);
-                        break;
+                        else{
+                            loc = fopen(strcat(document_directory, argument), "r");
+                        }   
+                        if(loc == NULL) {   
+                            res = "HTTP/1.1 404 File Not Found\r\n\r\n";
+                            /* write message to the connection */
+                            if(write (sd2, res,strlen (res)) < 0)
+                                errexit ("error writing message: %s", res);
+                            break;
+                        }
+                        else
+                        {
+                            //200 ok land
+                            res = "HTTP/1.1 200 OK\r\n\r\n";
+                            if(write (sd2, res,strlen (res)) < 0)
+                                errexit ("error writing message: %s", res);
+                            char buffer[BUFLEN]; //temporary buffer to store binary data pieces
+                            size_t bufferSize = sizeof(buffer);
+                            size_t bytesRead = 0; //keep track of the bytesRead
+                            while(!feof(loc)) //as long as we are not at the end of the file for socket descriptor, binary data need to be continuous read and written
+                            {
+                                bytesRead = fread(buffer, 1, bufferSize, loc); //read one byte of data from socket descriptor file
+                                if(bytesRead > 0) //got info to write because bytesRead is greater than 0
+                                {
+                                    send(sd2, buffer, bytesRead, 0); //write 1 byte of data to file
+                                }   
+                            }
+                            fclose(loc);
+                            break;
+                        }
                     }
                 }
-            }
-            else{
-                //invalid?
+                else{
+                    //invalid?
+                }
             }
         }
-    }
-   //}
-    /* close the current TCP connection*/
-    close (sd2);
-    /* close listening connection and exit */
-    //close (sd);
-    //exit (0);
+    //}
+        /* close the current TCP connection*/
+        close (sd2);
+        /* close listening connection and exit */
+        //close (sd);
+        //exit (0);
    }
 }
 bool isPortInRange(char* port)
